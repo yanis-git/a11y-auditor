a9f7af569a8349088caaa9f3c7e775c95ac82914
yanis <yanis@ipedis.com>
2022-04-17 09:55:02 +0400

    feat: add describer id reference checker

---
+++ src/auditor/rule/runners/describer/isDescribedByIsReferToValidElement.checker.ts
@@ -0,0 +1,35 @@
+import {Page} from 'puppeteer';
+import {RuleResult} from '../../results/rule-result';
+
+const ruleName = 'DescribeBy refer to a valid id ?';
+const ruleDescription = 'when you use describedBy, you should refer to a valid id, present on the page';
+const sources = [
+    "https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-labelledby",
+    "https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-describedby",
+];
+
+export async function isImagesHaveAltChecker(page: Page): Promise<RuleResult> {
+    // parsing section
+    const results = await page.evaluate(() => {
+        const elements = document.querySelectorAll<HTMLElement>('[aria-describedby], [aria-labelledby]');
+        return Array
+            .from(elements)
+            .filter((element: HTMLElement) => {
+                const id = element.getAttribute('aria-describedby') || element.getAttribute('aria-labelledby');
+                const idIsProvided = id !== null && id !== '';
+                return (
+                    idIsProvided &&
+                    // must be present once on the page.
+                    document.querySelectorAll(id).length === 1
+                );
+            })
+            .map(element => (element.getAttribute('aria-describedby') || element.getAttribute('aria-labelledby') as string));
+        });
+
+    // decision section
+    if (results.length === 0) {
+        return RuleResult.createSuccessRecommandation(ruleName, ruleDescription);
+    } else {
+        return RuleResult.createErrorRecommandation(ruleName, ruleDescription, results, sources);
+    }
+}
