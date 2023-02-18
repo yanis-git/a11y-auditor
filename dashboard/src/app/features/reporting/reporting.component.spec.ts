8e72bccfd50ea3f1a632f79419b57cc20a2a8fbd
yanis <yanis@ipedis.com>
2023-02-19 00:07:39 +0400

    feat(reporting) fix warning on test

--- dashboard/src/app/features/reporting/reporting.component.spec.ts
+++ dashboard/src/app/features/reporting/reporting.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReportingComponent } from './reporting.component';
 import {RuleResultStore} from '../api/rule-result-store';
 import {HttpClientModule} from '@angular/common/http';
+import {IntroComponent} from "./ui/intro/intro.component";
+import {RenderComponent} from "./containers/render/render.component";
+import {FilterComponent} from "./containers/filter/filter.component";
 
 describe('ReportingComponent', () => {
   let component: ReportingComponent;
@@ -10,7 +13,7 @@ describe('ReportingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReportingComponent ],
+      declarations: [ ReportingComponent, IntroComponent, RenderComponent, FilterComponent ],
       providers: [RuleResultStore],
       imports: [HttpClientModule]
     })
